46db77bc2d6bcea0fb82473dfee9521186ff23df
Sara <sara.mara.diaz@proton.me>
2026-08-21 12:44:25 +0100

    responsive phone

--- src/app/layout.js
+++ src/app/layout.js
@@ -78,6 +78,13 @@ export const metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
